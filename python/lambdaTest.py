694f98936dc6b1e0624c4e64a0b3934927f062ac
Coco <782533807@qq.com>
2015-11-09 10:27:40 +0800

    #update lambdaTest

--- python/lambdaTest.py
+++ python/lambdaTest.py
@@ -30,7 +30,7 @@ def makeActions():
     return acts
 
 acts = makeActions()
- # 只记录最后一次循环i的值
+
 print(acts[0](2))
 print(acts[1](2))
 print(acts[2](2))
@@ -45,5 +45,51 @@ def appendX():
     return acts2
 
 print(appendX())
-    
+
+def makeActions2():
+    acts = []
+    for i in range(5):
+        acts.append(lambda x, i = i:i**x )
+    return acts
+
+acts3 = makeActions2()
+print(acts3[0](2))
+print(acts3[1](2))
+print(acts3[2](2))
+print(acts3[3](2))
+print(acts3[4](2))
+
+def f1():
+    x = 99
+    def f2():
+        def f3():
+            print(x)
+        f3()
+    f2()
+
+f1()
+
+
+def tester(start):
+    state = start
+    def nested(label):
+        print(label,state)
+    return nested
+
+F = tester(0)
+print(F('spam'))
+
+# print(F(0)('spam'))
+
+def tester1(start):
+    state = start
+    def nested(label):
+        nonlocal state
+        print(label,state)
+        state += 1
+    return nested
+
+F1 = tester1(0)
+print(F1("spam"))
+
 
